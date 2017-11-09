3e47b7765592e1c17a6dacf41ec33351b7a0ef10
njs6145 <njs6145@g.rit.edu>
2017-11-09 17:19:28 -0500

    Fixed package and other code

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -40,17 +40,17 @@ const makeDomo = (req, res) => {
   return domoPromise;
 };
 
-const getDomos = (request,response) =>{
+const getDomos = (request, response) => {
   const req = request;
   const res = response;
-  
-  return Domo.DomoModel.findByOwner(req.session.account._id, (err,docs) =>{
-    if (err){
+
+  return Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
       console.log(err);
-      return res.status(400).json({error: 'An error occurred'});
+      return res.status(400).json({ error: 'An error occurred' });
     }
-    
-    return res.json({domos: docs});
+
+    return res.json({ domos: docs });
   });
 };
 
